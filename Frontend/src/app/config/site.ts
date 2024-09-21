9cd7190663a1d875316284e4d6ccb5651045f9e1
sahilkakwani9 <sahkakwani@gmail.com>
2024-09-21 15:55:53 +0800

    fixes

---
+++ Frontend/src/app/config/site.ts
@@ -0,0 +1,43 @@
+import { SidebarNavItem, SiteConfig } from "../types";
+
+export const siteConfig: SiteConfig = {
+  name: "Blooping",
+  description: "Stake your wstEth and earn unbelievable yield",
+  url: "localhost:3000",
+  ogImage: `localhost:3000/_static/og.jpg`,
+  links: {
+    twitter: "",
+    github: "",
+  },
+  mailSupport: "support@blooping.com",
+};
+
+export const footerLinks: SidebarNavItem[] = [
+  {
+    title: "Company",
+    items: [
+      { title: "About", href: "#" },
+      { title: "Enterprise", href: "#" },
+      { title: "Terms", href: "/terms" },
+      { title: "Privacy", href: "/privacy" },
+    ],
+  },
+  {
+    title: "Product",
+    items: [
+      { title: "Security", href: "#" },
+      { title: "Customization", href: "#" },
+      { title: "Customers", href: "#" },
+      { title: "Changelog", href: "#" },
+    ],
+  },
+  {
+    title: "Docs",
+    items: [
+      { title: "Introduction", href: "#" },
+      { title: "Installation", href: "#" },
+      { title: "Components", href: "#" },
+      { title: "Code Blocks", href: "#" },
+    ],
+  },
+];
